d102a73916ed1d150e462e7b399ef7c19dbf6625
guixiangyu <guixiangyu1@gmail.com>
2019-09-05 20:12:46 +0800

    init

--- evaluate.py
+++ evaluate.py
@@ -34,7 +34,7 @@ def main():
                                    processing_tag=config.processing_tag)
 
     # evaluate and interact
-    print(test4cl)
+    print(len(test4cl))
     model.evaluate(test4cl)
     # interactive_shell(model)
 
